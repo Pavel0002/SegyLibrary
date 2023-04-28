9c9d2f83f7f6ebb26ad96994f12ea2ec51c5a6ad
Pavel0002 <Paul0002@rambler.ru>
2023-04-28 13:03:10 +0400

    master SegyLibrary: project file names consistency improvements

--- SegyLibrary/SegySamplesExtractorInserterTest/Properties/AssemblyInfo.cs
+++ SegyLibrary/SegySamplesExtractorInserterTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SegyInserterDeleterTest")]
+[assembly: AssemblyTitle("SegySamplesExtractorInserterTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SegyInserterDeleterTest")]
+[assembly: AssemblyProduct("SegySamplesExtractorInserterTest")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
